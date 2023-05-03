8671b441c396c2c367ba1709208cf8897fd21b1a
NewAnton <anton1korobov1@gmail.com>
2023-05-03 19:03:18 +0300

    feat: add icons

--- src/components/Vacancies/VacancyCard/VacancyCard.tsx
+++ src/components/Vacancies/VacancyCard/VacancyCard.tsx
@@ -1,29 +1,34 @@
 import styles from './VacancyCard.module.scss';
 
 import React from 'react';
-
 import Link from 'next/link';
 import Image from 'next/image';
+import { clsx } from 'clsx';
+
 import FavoriteButton from '@/components/FavoriteButton/FavoriteButton';
 
 export function VacancyCard() {
   return (
     <Link className={styles.vacancyCard} href={'/'}>
-      <h3 className={styles.title}>Менеджер-дизайнер</h3>
-      <FavoriteButton isActive={false} />
+      <div className={styles.header}>
+        <h3 className={styles.title}>Менеджер-дизайнер</h3>
+        <FavoriteButton isActive={false} />
+      </div>
       <div className={styles.info}>
-        <div className={styles.salary}>
-          <p>
-            <b>з-п от 2000 byn</b>
-          </p>
-        </div>
-        <div className={styles.type}>
-          <p>Полный рабочий день</p>
-        </div>
-        <div className={styles.location}>
-          <p>Icon</p>
-          <p>Минск</p>
-        </div>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <p className={styles.salary}>
+              <b>з-п от 2000 byn</b>
+            </p>
+          </li>
+          <li className={styles.item}>
+            <p className={styles.type}> • Полный рабочий день</p>
+          </li>
+          <li className={clsx(styles.item, styles.location)}>
+            <Image src={'/icons/location.svg'} width={16} height={18} alt={''} />
+            <p className={styles.locationName}>Минск</p>
+          </li>
+        </ul>
       </div>
     </Link>
   );
